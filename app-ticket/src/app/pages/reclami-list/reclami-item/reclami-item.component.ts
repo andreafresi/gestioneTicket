d60a7d1955eb1b554ae7ecc9ff4ff5778dbfe08d
Lorenzo Tagliani <tagliani46@gmail.com>
2024-03-27 16:45:47 +0100

    Voglio morire

--- app-ticket/src/app/pages/reclami-list/reclami-item/reclami-item.component.ts
+++ app-ticket/src/app/pages/reclami-list/reclami-item/reclami-item.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./reclami-item.component.scss']
 })
 export class ReclamiItemComponent {
+  rows = [
+    { id: 1, name: 'John', age: 30 },
+    { id: 2, name: 'Jane', age: 25 },
+    { id: 3, name: 'Doe', age: 40 }
+  ];
 
+  columns = [
+    { prop: 'id', name: 'ID' },
+    { prop: 'name', name: 'Name' },
+    { prop: 'age', name: 'Age' }
+  ];
 }
+  
