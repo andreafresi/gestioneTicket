4c2bee010a8f71e67dbe4d2858fc79cf76eca2eb
Dorian Bala <baladorian9@gmail.com>
2024-03-27 14:31:02 +0100

    aggiunto run_Mock

--- app-ticket/src/app/shared/services/reclami.service.ts
+++ app-ticket/src/app/shared/services/reclami.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { Cliente } from '../models/cliente';
+import { Reclamo } from '../models/reclamo';
+import { Negozio } from '../models/negozio';
+import { Regione } from '../models/regione';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +12,22 @@ export class ReclamiService {
 
   constructor(private readonly httpClient: HttpClient) { }
 
+  getClienti(){
+    return this.httpClient.get<Cliente[]>('http://localhost:3000/cliente')
+  }
+
+  getReclami(){
+    return this.httpClient.get<Reclamo[]>('http://localhost:3000/reclamo')
+  }
+
+  getNegozi(){
+    return this.httpClient.get<Negozio[]>('http://localhost:3000/negozio')
+  }
+
+  getRegioni(){
+    return this.httpClient.get<Regione[]>('http://localhost:3000/regione')
+  }
+
+  
 
 }
