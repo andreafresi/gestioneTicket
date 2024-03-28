160383c5b6a64384ebc75baa00f84a3eb1ec6be2
MariaAndreFresi <manf.ken@gmail.com>
2024-03-28 14:29:05 +0100

    aggiunte

--- app-ticket/src/app/pages/reclami-list/reclami-list.component.ts
+++ app-ticket/src/app/pages/reclami-list/reclami-list.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Reclamo } from 'src/app/shared/models/reclamo';
+import { ReclamiService } from 'src/app/shared/services/reclami.service';
 
 @Component({
   selector: 'app-reclami-list',
@@ -6,5 +10,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./reclami-list.component.scss']
 })
 export class ReclamiListComponent {
+  reclamo: Reclamo[] = [];
+
+  constructor(
+    private activeRoute:ActivatedRoute,
+    private router: Router,
+    private reclamiService: ReclamiService,
+    private formBuilder: FormBuilder
+  ){}
+
+
+  ricercaForm: FormGroup = this.formBuilder.group({
+    codice: [''],
+    descrizione: [''],
+    stato: [''],
+    gestione: [''],
+    data: [''],
+    area: [''],
+    causale: [''],
+    idReclamo: [''],
+    nome: [''],
+    cognome: [''], 
+    banner: [''],
+  })
+
+  ngOnInit(): void {
+    this.reclamiService.getClienti();
+    this.reclamiService.getReclami();
+  }
+/*
+  getClienti() {
+    this.reclamiService.getClienti().subscribe((res) => {
+      this.listClienti = res;
+    });
+  }
+
+  getReclami() {
+    this.reclamiService.getReclami().subscribe((res) => {
+      this.listReclami = res;
+    });
+  } */
 
 }
