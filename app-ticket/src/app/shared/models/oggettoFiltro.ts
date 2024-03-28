1516527625bac2f04d9a7f84cbabcc47a3406c93
Dorian Bala <baladorian9@gmail.com>
2024-03-28 16:52:57 +0100

    commit.

---
+++ app-ticket/src/app/shared/models/oggettoFiltro.ts
@@ -0,0 +1,12 @@
+export interface OggettoFiltro {
+  codice?: string;
+  descNegozio?: string;
+  stato?: string;
+  gestione?: string;
+  data?: string;
+  area?: string;
+  causale?: string;
+  idReclamo?: string;
+  nome?: string;
+  cognome?: string;
+}
